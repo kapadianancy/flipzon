d6e8deb1d26b0fb5a74cbc7a7911a9638c15a45e
chavda-bhavik <bhavik.chvda@gmail.com>
2020-09-22 10:37:57 +0530

    Modified names to stop future conflict

--- api/controllers/ProductAdminController.js
+++ api/controllers/ProductAdminController.js
@@ -1,11 +1,11 @@
-const productService = require("../services/ProductService");
+const productService = require("../services/ProductAdminService");
 
 module.exports = (app) => {
-    app.get("/products", async (req, res, next) => {
+    app.get("/admin/products", async (req, res, next) => {
         let products = await productService.fetchProducts();
         res.send(products);
     })
-    app.post("/products", async (req, res, next) => {
+    app.post("/admin/products", async (req, res, next) => {
         try {
             let product = await productService.addProduct(req.body);
             res.send(product);
@@ -13,7 +13,7 @@ module.exports = (app) => {
             next(error);
         }
     })
-    app.put("/products/:id", async (req, res, next) => {
+    app.put("/admin/products/:id", async (req, res, next) => {
         try {
             let product = await productService.editProduct(req.params.id, req.body);
             res.send(product);
@@ -21,7 +21,7 @@ module.exports = (app) => {
             next(error);
         }
     })
-    app.delete("/products/:id", async (req, res, next) => {
+    app.delete("/admin/products/:id", async (req, res, next) => {
         try {
             let product = await productService.deleteProduct(req.params.id);
             res.send(product);
