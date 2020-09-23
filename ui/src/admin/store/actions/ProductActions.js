b5428ac6de81565928301f15c38074885df65094
chavda-bhavik <bhavik.chvda@gmail.com>
2020-09-23 18:20:22 +0530

    Added isDeleted to models, description to product and cors

--- ui/src/admin/store/actions/ProductActions.js
+++ ui/src/admin/store/actions/ProductActions.js
@@ -1,11 +1,11 @@
 import * as types from '../Types'
-import axios from '../../axios'
+import axios from 'axios'
 
 export const fetchProducts = () => {
     return async(dispatch) => {
         dispatch({ type: types.INIT_FETCH_PRODUCTS });
         try {
-            const products = await axios.get("products");
+            const products = await axios.get("http://localhost:8080/admin/products");
             console.log(products);
         } catch(error) {
             console.log(error);
